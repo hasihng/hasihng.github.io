0f367c8fd75d14c6dab63517009db9dc3032f8f4
hasihng <hasihng@gmail.com>
2016-08-30 10:39:41 +0400

    Update base.js

--- js/base.js
+++ js/base.js
@@ -1,5 +1,29 @@
+function serialize(object) {
+  var type = typeof object;
+  if (object === null) {
+    return '"nullValue"';
+  }
+  if (type == 'string' || type === 'number' || type === 'boolean') {
+    return '"' + object + '"';
+  } else if (type === 'function') {
+    return '"functionValue"';
+  } else if (type === 'object') {
+    var output = '{';
+    for (var item in object) {
+      if (item !== 'enabledPlugin') {
+        output += '"' + item + '":' + serialize(object[item]) + ',';
+      }
+    }
+    return output.replace(/\,$/, '') + '}';
+  } else if (type === 'undefined') {
+    return '"undefinedError"';
+  } else {
+    return '"unknownTypeError"';
+  }
+};
+
 $( document ).ready(function() {
     $(".user-agent").html(navigator.userAgent);
-    $(".details p").html("navigator: " + String(navigator) + "<br />devicePixelRatio: " + window.devicePixelRatio);
+    $(".details p").html(serialize(navigator));
     console.debug(navigator);
 });
